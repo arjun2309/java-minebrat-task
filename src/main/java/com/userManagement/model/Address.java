8230940de902c456f1f7130f00b485d610d2833d
arjun2309 <163283784+arjun2309@users.noreply.github.com>
2025-09-17 12:40:18 +0530

    Address.java

--- src/main/java/com/userManagement/model/Address.java
+++ src/main/java/com/userManagement/model/Address.java
@@ -7,8 +7,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "addresses")
 public class Address {
     @Id @GeneratedValue
     private Long id;
