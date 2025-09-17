192f20845c80fa4dd1590333e4574191396ae765
arjun2309 <163283784+arjun2309@users.noreply.github.com>
2025-09-17 12:41:07 +0530

    User.java

--- src/main/java/com/userManagement/model/User.java
+++ src/main/java/com/userManagement/model/User.java
@@ -7,8 +7,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users") 
 public class User {
     @Id @GeneratedValue
     private Long id;
